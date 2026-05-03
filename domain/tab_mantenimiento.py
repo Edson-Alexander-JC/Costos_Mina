4d4d2e029c66ce08577e243b7a9bc099b0de0af5
Edson-Alexander-JC <edsonalexanderjaracantu@gmail.com>
2026-05-02 22:29:21 -0500

    Err del Err en la instanciacion

--- domain/tab_mantenimiento.py
+++ domain/tab_mantenimiento.py
@@ -3,39 +3,41 @@ from interfaces.tab import Tab
 
 class TabMantenimiento(Tab):
 
-    def __init__(self,data:dict):
+    def __init__(self, data: dict):
         self.col1 = data[0]
         self.col2 = data[1]
-        self.col3 = data[3]
+        self.col3 = data[2]
 
     def render(self):
-            st.subheader("Mantenimiento")
+        st.subheader("Mantenimiento")
 
-            col1, col2, col3 = st.columns([1, 2,3])
-            with col1:
-                self.set_col1(col1[0],col1[1])
-                calcular = st.button("Calcular Operación", key="btn_op")
+        col1, col2, col3 = st.columns([1, 2, 3])
 
-            with col2:
-                self.set_col2(col2[0],col2[1])
-            with col3:
-                self.set_col3(col3[0],col3[1])
+        with col1:
+            self.set_col1(self.col1[0], self.col1[1])
+            st.button("Calcular Operación", key="btn_op")
 
+        with col2:
+            self.set_col2(self.col2[0], self.col2[1])
+
+        with col3:
+            self.set_col3(self.col3[0], self.col3[1])
 
     def set_inputs(self, title: str, label_value: list):
         st.markdown(title)
         for x in label_value:
-            st.number_input(x,key=x)
-        
-    def set_viewputs(title:str,label_value:dict):
+            st.number_input(x, key=x)
+
+    def set_viewputs(self, title: str, label_value: dict):
         st.markdown(title)
         for x in label_value:
-            st.metric(x,label_value[x])
-        
+            st.metric(x, label_value[x])
 
-    def set_col1(self,title:str,label_value:dict):
-        self.set_inputs(title,label_value)
-    def set_col2(self,title:str,label_value:dict):
-        self.set_mini_view(title,label_value)
-    def set_col3(self,title:str,label_value:dict):
-        self.set_mini_view(title,label_value)
+    def set_col1(self, title: str, label_value: list):
+        self.set_inputs(title, label_value)
+
+    def set_col2(self, title: str, label_value: dict):
+        self.set_viewputs(title, label_value)
+
+    def set_col3(self, title: str, label_value: dict):
+        self.set_viewputs(title, label_value)
